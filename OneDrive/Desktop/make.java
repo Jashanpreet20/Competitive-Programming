b629c61693ca9c02efaa8bf548ab71e88f40eafa
Jashanpreet20 <jashankot20@gmail.com>
2023-10-05 16:19:18 +0530

    binary to decimal conversion

--- OneDrive/Desktop/make.java
+++ OneDrive/Desktop/make.java
@@ -1,31 +1,17 @@
 import java.util.*;
 
 class make{
-    public static int convert(int n){
-
-        int binary=0;
+    public static int convert(int n){   
+        int decimal=0;
         int i=0;
 
-        while(true){
-            if(n==0)
-            {
-                break;
-            }else{
-                int digit=n % 2;
-                binary += digit* (int)Math.pow(10, i++);
-                n = n /2;
-            }
+        while(n !=0 ){
+            int digit=n % 10;
+
+            decimal += digit * (int) Math.pow(2, i++);
+            n = n/10;
         }
-        return binary;
-        // fast bitwise operator
-        // int binary=0;
-        // int i=0;
-        //  while ( n != 0){
-        //     int digit=n & 1;
-        //     binary += digit *(int) Math.pow(10, i++);
-        //      n >>=1;
-        //  }
-        //  return binary;
+        return decimal;
     }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
